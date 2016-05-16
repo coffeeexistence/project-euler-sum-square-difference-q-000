a8d5e4cd73326f218bb7d532f59ac2f11fb8db0a
John Anthony Rivera <askinganthony@gmail.com>
2016-05-15 23:04:33 -0700

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+
+class SumSquareDifference
+  attr_accessor :limit
+
+  def initialize(limit)
+    self.limit = limit
+  end
+
+  def difference
+    sum_of_squares = (1..self.limit).inject {|sum, n| sum + (n * n)}
+
+    sum = (1..self.limit).inject {|sum, n| sum + n}
+    square_of_sum = sum * sum
+
+    square_of_sum - sum_of_squares
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+
+def sum_square_difference(limit)
+  sum_of_squares = (1..limit).inject {|sum, n| sum + (n * n)}
+
+  sum = (1..limit).inject {|sum, n| sum + n}
+  square_of_sum = sum * sum
+
+  square_of_sum - sum_of_squares
+end
